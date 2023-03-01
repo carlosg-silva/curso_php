76bac4e67ef32c188cc5671ea1c9dab70bc55f7f
Carlos <cgomessilva2015@gmail.com>
2023-03-01 11:42:52 -0300

    fim secao 2 conceitos basicos

---
+++ 2_conceitos_basicos/1_case_sensitive/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//Não é case sensitive
+echo "Teste <br>";
+eChO "Testando 2 <br>";
+ECHO "Teste 3 <br>";
+
+//É case sensitive
+$nome = "Carlos";
+$NOME = "Teste";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
+
+?>
+
+
+
---
+++ 2_conceitos_basicos/5_exercicio_2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Esta variável contém o nome da pessoa
+$nome = "Carlos";
+
+// Esta variável contém a idade da pessoa
+$idade = 29;
+
+// Esta variável contém a profissão da pessoa
+$profissao = "Programador";
+
+echo $profissao;
+
+?>
