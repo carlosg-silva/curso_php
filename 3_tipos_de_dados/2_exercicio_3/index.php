2f073a69745509f2f92fb340c38f005f1ded5aaf
Carlos <cgomessilva2015@gmail.com>
2023-03-01 17:37:40 -0300

    strings

---
+++ 3_tipos_de_dados/2_exercicio_3/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 5;
+echo '<br>';
+echo -12;
+echo '<br>';
+echo 25;
+
+?>
---
+++ 3_tipos_de_dados/6_exercicio_4/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 12.5;
+$b = 325.54;
+$c = 1.3;
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+
+if (is_float($c)) {
+  echo "É float";
+}
