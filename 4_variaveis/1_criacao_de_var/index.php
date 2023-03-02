c88a6886b867443e888dae4774c613a272dc7f63
Carlos <cgomessilva2015@gmail.com>
2023-03-02 17:25:04 -0300

    secao 4 aula - var de var

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$teste = "Algum testo";
+
+echo $teste;
+
+$num = 10;
+
+echo "<br>";
+echo $num;
+
+$_nome = "Carlos";
+
+echo "<br>";
+echo $_nome;
+
+echo "<br>";
+
+$velocidadeMaxima = 100;
+$velocidade_minima = 100;
---
+++ 4_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,10 @@
+<?php
+$num = 10; // int
+$str = "Carlos"; // string
+$arr = ['manga', 'maça', 'uva']; // array
+
+echo $num;
+echo "<br>";
+echo $str;
+echo "<br>";
+print_r($arr);
