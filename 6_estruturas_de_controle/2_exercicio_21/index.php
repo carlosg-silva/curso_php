7e5441faed95ce06ae1a06f9dda49dba83674053
Carlos <cgomessilva2015@gmail.com>
2023-03-05 22:28:01 -0300

    estrutura if

---
+++ 6_estruturas_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// 5 é maior que 2?
+$a = 5;
+$b = 2;
+if ($a > $b) {
+  echo "Entrou no if 1 <br>";
+}
+
+//Matheus é diferente de Pedro?
+$c = "Matheus";
+$d = "Pedro";
+if ($c != $d) {
+  echo "Entrou no if 2 <br>";
+}
+
+//12 é menor ou igual a 11
+$e = 12;
+$f = 11;
+if ($e <= $f) {
+  echo "Entrou no if 3 <br>";
+}
---
+++ 6_estruturas_de_controle/3_exercicio_22/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$idade = 20;
+$maiorIdade = 18;
+
+if ($idade >= $maiorIdade) {
+  echo "O usuário é maior de idade! <br>";
+}
+
+$idade = 15;
+if ($idade <= $maiorIdade) {
+  echo "O usuário é menor de idade! <br>";
+}
