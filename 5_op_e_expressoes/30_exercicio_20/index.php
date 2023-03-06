b69864e7454e99e0f9610eb0e9a8c7f90d0e1a93
Carlos <cgomessilva2015@gmail.com>
2023-03-05 21:32:25 -0300

    fim secao operadores e expressoes

---
+++ 5_op_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = 20;
+$b = 2;
+
+echo $a <= $b ? "A variável A é menor ou igual a variável B" : "A variável A não é menor ou igual a variável B <br>";
+
+echo $a >= $b ? "A variável A é maior ou igual a variável B" : "A variável A não é maior ou igual a variável B <br>";
