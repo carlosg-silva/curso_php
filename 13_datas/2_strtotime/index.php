599f9ca88f9826bccab576ae706f4be3b00ec309
Carlos <cgomessilva2015@gmail.com>
2023-04-16 22:01:49 -0300

    secao datas aula strtotime

---
+++ 13_datas/2_strtotime/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$cincoDias = strtotime("5 days");
+
+echo $cincoDias . "<br>";
+
+$dezDias = strtotime("10 days");
+
+echo $dezDias . "<br>";
+
+$dataAtualMais5 = date('d/m/y', $cincoDias);
+
+echo $dataAtualMais5 . "<br>";
+
+$dataAtualMais10 = date('d/m/y', $dezDias);
+
+echo $dataAtualMais10 . "<br>";
+
+$doisMeses = strtotime('2 months');
+
+echo $doisMeses . "<br>";
+
+$dataAtualMaisDoisMeses = date('d/m/y', $doisMeses);
+
+echo $dataAtualMaisDoisMeses . "<br>";
+
+$dozeAnos = strtotime('12 years');
+
+echo $dozeAnos . "<br>";
+
+echo date('d/m/y',  $dozeAnos);
