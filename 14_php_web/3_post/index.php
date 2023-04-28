af2a6b7d979fad2081cad5668bb7e084f8bf54fc
Carlos <cgomessilva2015@gmail.com>
2023-04-28 16:44:45 -0300

    secao web aula auto processamento

--- 14_php_web/3_post/index.php
+++ 14_php_web/3_post/index.php
@@ -9,25 +9,26 @@
 </head>
 
 <body>
-  <form action="cadastro.php" method="POST"></form>
-  <div>
-    <input type="text" name="marca" placeholder="Marca do carro">
-  </div>
-  <div>
-    <input type="text" name="preco" placeholder="Preço do carro">
-  </div>
-  <div>
-    <input type="checkbox" name="opcionais[]" value="Teto solar"> Teto solar
-  </div>
-  <div>
-    <input type="checkbox" name="opcionais[]" value="Película"> Película
-  </div>
-  <div>
-    <input type="checkbox" name="opcionais[]" value="Blindado"> Blindado
-  </div>
-  <div>
-    <input type="submit" value="Enviar">
-  </div>
+  <form action="cadastro.php" method="POST">
+    <div>
+      <input type="text" name="marca" placeholder="Marca do carro">
+    </div>
+    <div>
+      <input type="text" name="preco" placeholder="Preço do carro">
+    </div>
+    <div>
+      <input type="checkbox" name="opcionais[]" value="Teto solar"> Teto solar
+    </div>
+    <div>
+      <input type="checkbox" name="opcionais[]" value="Película"> Película
+    </div>
+    <div>
+      <input type="checkbox" name="opcionais[]" value="Blindado"> Blindado
+    </div>
+    <div>
+      <input type="submit" value="Enviar">
+    </div>
+  </form>
 </body>
 
 </html>
